179d0a5d753b46ab05d7e87a998ff90bdabc1769
Frederik Ehmsen <Freehm01@easv365.dk>
2026-08-31 11:00:12 +0200

    added gitignore to database

--- API/Program.cs
+++ API/Program.cs
@@ -6,7 +6,6 @@ using Service;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IPetService,PetService>();
-builder.Services.AddSingleton<PetshopDB>();
 
 var connectionString = "Data Source=dev.db";
 var options = new DataOptions().UseSQLite(connectionString);
@@ -15,15 +14,29 @@ var dataOptions = new DataOptions<PetshopDB>(options);
 builder.Services.AddScoped<PetshopDB>(_ => new PetshopDB(dataOptions));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    scope
+        .ServiceProvider
+        .GetRequiredService<PetshopDB>()
+        .CreateTable<Pet>(tableOptions: TableOptions.CreateIfNotExists);
+}
 app.MapControllers();
 app.Run();
 
 
-public class MyPetshopController(PetService petService) : ControllerBase
+public class MyPetshopController(PetService petService, PetshopDB db) : ControllerBase
 {
     [HttpGet(nameof(GetPets))]
     public List<Pet> GetPets()
     {
+        var pet = new Pet()
+        {
+            Id = "hamburger" + new Random().Next(),
+            Name = "Hamburger"
+        };
+        db.Insert(pet);
         return petService.GetPets();
     }
 }
